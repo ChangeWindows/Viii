5a4fbd41a345f1cb134b84e1c38e70de3e41bceb
Yannick <yannick.franssen@outlook.com>
2017-06-30 23:07:41 +0200

    Use models to get the ring/platform name

--- app/Release.php
+++ app/Release.php
@@ -3,76 +3,28 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Platform;
+use App\Ring;
 
 class Release extends Model
 {
     protected $fillable = ['build_id', 'build_string', 'platform', 'ring'];
 
     function getPlatformName() {
-        switch( $this->id ) {
-            case 1:
-                return 'PC';
-            case 2:
-                return 'Mobile';
-            case 3:
-                return 'Xbox';
-            case 4:
-                return 'Server';
-            case 5:
-                return 'IoT';
-            case 6:
-                return 'Team';
-            case 7:
-                return 'Mixed Reality';
-        }
+        return Platform::find( $this->id )->name;
     }
 
     function getRingName() {
-        switch( $this->id ) {
-            case 1:
-                return 'vNext';
-            case 2:
-                switch( $this->platform ) {
-                    case 3:
-                        return 'Alpha Ring';
-                    default:
-                        return 'Fast Ring';
-                }
+        switch( $this->platform ) {
             case 3:
-                switch( $this->platform ) {
-                    case 3:
-                        return 'Beta Ring';
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                        return 'Preview';
-                    default:
-                        return 'Slow Ring';
-                }
+                return Ring::find( $this->ring )->xbox_name;
             case 4:
-                return 'Ring 3';
             case 5:
-                switch( $this->platform ) {
-                    case 3:
-                        return 'Ring 4';
-                    default:
-                        return 'Release Preview Ring';
-                }
             case 6:
-                switch( $this->platform ) {
-                    case 3:
-                    case 5:
-                    case 6:
-                    case 7:
-                        return 'Production';
-                    default:
-                        return 'Semi-Annual Pilot Channel';
-                }
             case 7:
-                return 'Semi-Annual Broad Channel';
-            case 8:
-                return 'Long-Term Servicing Channel';
+                return Ring::find( $this->ring )->other_name;
+            default:
+                return Ring::find( $this->ring )->default_name;
         }
     }
 
