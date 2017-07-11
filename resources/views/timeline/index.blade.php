05a1c80e21fceae7db38ec1530d9e44cb3a09a26
Yannick <yannick.franssen@outlook.com>
2017-07-11 23:23:34 +0200

    Show information in a grid in the timeline

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -6,7 +6,13 @@
         @foreach ( $releases as $release )
             <div class="col-md-12">
                 <div class="row list-bar">
-                    <a class="col-12 list-bar-item list-bar-default" href="{{ route('editRelease', ['id' => $release->id]) }}">{{ $release->getPlatformName().' '.$release->getString().' '.$release->getRingName() }}</a>
+                    <a class="col-12 list-bar-item list-bar-default" href="{{ route('showBuild', ['id' => $release->build]) }}">
+                        <div class="row list-bar-item-row">
+                            <div class="col-1">{{ $release->getPlatformName() }}</div>
+                            <div class="col-5">{{ $release->getString() }}</div>
+                            <div class="col-6"><span class="label {{ $release->getRingName( 'class' ) }}">{{ $release->getRingName( 'short' ) }}</span></div>
+                        </div>
+                    </a>
                 </div>
             </div>
         @endforeach
