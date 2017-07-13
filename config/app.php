7fb6c69c3f3742569a6388bcf7366f4e93bb7e86
Yannick <yannick.franssen@outlook.com>
2017-07-13 10:14:15 +0200

    Add the Laravel Debugbar

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
