c228a53ffe05765331759db71b7d6ac4e7f26464
Yannick <yannick.franssen@outlook.com>
2017-09-07 21:56:53 +0200

    Revert "Small cleanup"
    This reverts commit 0ed07a5bedcb6b185a3e09612c79a5c37fbe7424.

--- app/Http/Controllers/BuildController.php
+++ app/Http/Controllers/BuildController.php
@@ -22,9 +22,19 @@ class BuildController extends Controller
 
         return view( 'backstage.build.index', compact( 'builds', 'current_milestone' ) );
     }
-    
-    public function edit( Build $build ) {
-        return view( 'backstage.build.edit', compact( 'build' ) );
+
+    public function show( Build $build ) {
+        $flights = Flight::join( 'deltas', 'flights.delta_id', '=', 'deltas.id')
+                ->select( ['flights.id', 'flights.ring_id', 'flights.delta_id', 'deltas.build_id', 'deltas.delta', 'deltas.platform_id'] )
+                ->where( 'build_id', $build->id )
+                ->orderBy( 'deltas.delta', 'desc' )
+                ->orderBy( 'deltas.platform_id', 'asc' )
+                ->orderBy( 'flights.ring_id', 'asc' )
+                ->paginate( 100 );
+
+        $current_delta = 0;
+            
+        return view( 'backstage.build.show', compact( 'build', 'flights', 'current_delta' ) );
     }
 
     public function delete( Build $build ) {
