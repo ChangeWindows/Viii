be23436b6c065fc25c0aec4d89b8f717d0fc5fed
Yannick <yannick.franssen@outlook.com>
2018-02-20 23:54:35 +0100

    Update Font Awesome and Bootstrap

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -16,9 +16,9 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/fontawesome-pro-core.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/fontawesome-pro-light.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/fontawesome-pro-brands.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/fontawesome.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/fa-light.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/fa-brands.min.css') }}" rel="stylesheet">
     </head>
     <body class="light">
         <div class="container">
